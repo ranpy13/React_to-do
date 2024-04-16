47426006117cd5069a1c37f6db70a543383c032f
Nandini Gour <nandinigour@Nandinis-MacBook-Air.local>
2024-04-16 15:20:46 +0530

    bug fix

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -14,18 +14,18 @@ const Testimonials = () => {
     if (tx > -50) {
       tx -= 25;
     } else tx = 0;
-    }
 
     slider.current.style.transform = `translateX(${tx}%)`;
-  };
+  }
+
   const slideBackward = () => {
     if (tx < 0) {
       tx += 25;
     } else tx = -50;
-    }
+    
 
     slider.current.style.transform = `translateX(${tx}%)`;
-  };
+  }
 
   return (
     <div className="testimonials">
@@ -110,6 +110,6 @@ const Testimonials = () => {
       </div>
     </div>
   );
-};
 
+  };
 export default Testimonials;
